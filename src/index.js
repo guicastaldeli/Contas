53a4dc9d10a283acacf13ca67fcc26d300908263
gui castaldeli <guicastaldeli8@gmail.com>
2024-10-03 10:10:43 -0400

    contas-update-test-1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './main';
+import App from './main.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './styles/style.css';
+
+function Main() {
+    return (
+        <p id='tst'>OIEEEEE!!!</p>
+    )
+}
+
+export default Main;
